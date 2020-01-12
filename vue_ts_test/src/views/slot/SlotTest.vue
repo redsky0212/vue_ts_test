73ca13c8de4f3e7598956c64c9dfc65c9bd8eccc
redsky <hyun0238@gmail.com>
2020-01-12 18:24:30 +0900

    node모듈설치

---
+++ vue_ts_test/src/views/slot/SlotTest.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Slot 테스트</h1>
+        <div></div>
+    </div>
+</template>
+
+<script>
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class SlotTest extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
