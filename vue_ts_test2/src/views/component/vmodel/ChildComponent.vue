3f08785f82131758adf00981506b4767d0d82004
redsky <hyun0238@gmail.com>
2020-01-14 21:55:24 +0900

    컴포넌트에 v-model 사용하기 예제추가

---
+++ vue_ts_test2/src/views/component/vmodel/ChildComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div>자식 컴포넌트</div>
+        <ul>
+            <li v-for="item of listData" :key="item.value" @click="onChange">
+                {{item.text}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop, Model} from 'vue-property-decorator';
+
+interface List {
+    value: string;
+    text: string;
+}
+
+@Component
+export default class ChildComponent extends Vue {
+    //@Prop() value!: List[];
+
+    @Model('click') readonly listData!: List[];
+    
+    private onChange(){
+        alert(1);
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_ts_test2/src/views/component/vmodel/ComponentVmodel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>component v-model 사용법</div>
+        <child-component v-model="listData"></child-component>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+import ChildComponent from './ChildComponent.vue';
+
+interface List {
+    value: string;
+    text: string;
+}
+
+@Component({
+    components: {
+        ChildComponent,
+    }
+})
+export default class ComponentVmodel extends Vue {
+    private readonly listData: List[] = [
+        {text:'첫번째', value:'1'},
+        {text:'두번째', value:'2'},
+        {text:'세번째', value:'3'},
+    ]; 
+}
+</script>
+
+<style>
+
+</style>
